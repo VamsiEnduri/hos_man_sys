0258075f98f392da186ad54cceb5ac382d514e8b
vamsiiiiiiii <vamsi10kcoders@gmail.com>
2026-02-27 15:10:54 +0530

    allowed hosts added

--- hos_man_sys/settings.py
+++ hos_man_sys/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5gd53hbk4jyznecdjo@i1bwl)m8_!*(qzv#0%gf#9$e*)+45=c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["hos-man-sys.onrender.com","localhost","127.0.0.1"]
 
 
 # Application definition
